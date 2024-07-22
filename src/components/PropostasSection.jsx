70cb47a955e25a53670251c4b2b31f2bdd298647
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-07-22 15:47:47 -0300

    [tereza] carrossel de infos

--- src/components/PropostasSection.jsx
+++ src/components/PropostasSection.jsx
@@ -1,15 +1,68 @@
 // src/components/PropostasSection.js
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const PropostasSection = ({ config }) => {
+  const { proposals, primary, secondary, text } = config.candidate || {};
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  };
+
   return (
-    <section id="propostas" className="p-6">
-      <h2 className="text-2xl font-bold mb-4">Propostas</h2>
-      {/* Adicione o conteúdo das propostas aqui */}
+    <section id="propostas" className="p-6" >
+      <h2 className="text-2xl font-bold mb-4 text-center" style={{color: primary}}>Propostas</h2>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {proposals && proposals.map((proposal, index) => (
+            <div key={index} className="p-4">
+              <div className="p-6 rounded-lg shadow-md" style={{ backgroundColor: secondary }} >
+                <h3 className="text-xl font-bold mb-2 text-center"  style={{color: text}}>{proposal.title}</h3>
+                <p className="text-base text-center"  style={{color: text}}>{proposal.description}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </section>
   );
 };
 
 export default PropostasSection;
-
-
