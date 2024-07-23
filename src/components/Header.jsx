15db67307130dfc3b11a8fa645a5bdac7933aa76
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-07-23 20:31:03 -0300

    [fix] header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,20 @@
 import React from 'react';
 
 const Header = ({ config }) => {
-  const { logoDark, secondary } = config.candidate || {};
+  const { logo, secondary, logoDark } = config.candidate || {};
 
   return (
-    <header className="text-white p-4 fixed w-full top-0 left-0 z-10" style={{ backgroundColor: '#e4e4e4' }}>
+    <header className="text-white p-4 fixed w-full top-0 left-0 z-10 border-b-2" style={{ backgroundColor: '#e4e4e4', borderColor: secondary }}>
       <nav className="container mx-auto flex justify-between items-center">
-        <ul className="flex space-x-4 items-center">
-          <li><a href="#home" className="hover:underline" style={{color: secondary}}>Home</a></li>
-          <li><a href="#propostas" className="hover:underline" style={{color: secondary}}>Propostas</a></li>
-          <li><a href="#contato" className="hover:underline" style={{color: secondary}}>Contato</a></li>
-        </ul>
-        {logoDark && (
-          <div className="flex items-center">
+        <div className="flex-1">
+          <ul className="flex space-x-4 items-center">
+            <li><a href="#home" className="hover:underline" style={{color: secondary}}>Home</a></li>
+            <li><a href="#propostas" className="hover:underline" style={{color: secondary}}>Propostas</a></li>
+            <li><a href="#contato" className="hover:underline" style={{color: secondary}}>Contato</a></li>
+          </ul>
+        </div>
+        {logo && (
+          <div className="flex-shrink-0">
             <img
               src={logoDark}
               alt="Logo"
